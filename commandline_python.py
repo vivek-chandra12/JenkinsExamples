e7f12fc5ec9790dfde8c38483fdab403ebbb649f
vivek-chandra12 <245122749012@mvsrec.edu.in>
2025-03-29 15:21:33 +0530

    Add files via upload

---
+++ commandline_python.py
@@ -0,0 +1,14 @@
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+r1=n1+n2;
+r2=n1-n2;
+r3=n1*n2;
+r4=n1/n2;
+
+print("Addition",r1)
+print("Subtraction",r2)
+print("Multiplication",r3)
+print("Division",r4)
+
+
